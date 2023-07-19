4a163ca54dd3d4adfb104d36290d3e065a66d857
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-07-19 23:05:30 +0900

    웹페이지와 연결

--- zimmyrabbit/zimmyrabbit/urls.py
+++ zimmyrabbit/zimmyrabbit/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
 from . import views
 
+app_name = 'zimmyrabbit'
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="index"),
+    path('check_model/', views.check_model, name="check_model"),
 ]
--- zimmyrabbit/zimmyrabbit/views.py
+++ zimmyrabbit/zimmyrabbit/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 def index(request) :
-    return HttpResponse('zimmyrabbit');
+    return render(request, 'jenkins/main.html')
+
+
+def check_model(request) :
+    content = request.POST.get('content')
+    scontent = sorted(set(content.split()))
+    print(scontent)
+    return redirect('zimmyrabbit:index')
