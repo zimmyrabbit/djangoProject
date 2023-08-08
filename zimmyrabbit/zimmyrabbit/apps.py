96c8e931b539a0958df87b736b000f3e939d817c
ZIMMYRABBIT\USER <zimmyrabbit@naver.com>
2023-08-09 08:07:14 +0900

    model수정

--- zimmyrabbit/zimmyrabbit/apps.py
+++ zimmyrabbit/zimmyrabbit/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.db.models import BigAutoField
 
 
 class ZimmyrabbitConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'zimmyrabbit'
--- zimmyrabbit/zimmyrabbit/models.py
+++ zimmyrabbit/zimmyrabbit/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 class BuildHist(models.Model):
+    env = models.CharField(max_length=50)
     component = models.CharField(max_length=50)
     lastupdtid = models.CharField(max_length=50)
-    lastupdt = models.DateTimeField()
+
+    def get_current_time():
+        return timezone.now
+    
+    lastupdt = models.DateTimeField(default=get_current_time)
+
+    class Meta:
+        unique_together = (("env", "component"),)
